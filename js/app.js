5ea2641eeaf642e01da26656316407503f851683
LinaAdamsson <studio.adamsson@gmail.com>
2026-04-21 15:15:57 +0200

    Added get req, scoreboard div, comments, todo:s

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Click a button or object to earn points so that I can increase my score.
 // See my current score during the game so that I know how well I am doing.
-
 // See a countdown timer so that I know how much time is left. setInterval();
 
 // Variables
@@ -17,66 +16,106 @@ const scoreDisplay = document.getElementById('scoreDisplay');
 const timerDisplay = document.getElementById('timerDisplay');
 const label1 = document.getElementById('label1');
 const input1 = document.getElementById('name');
+const scoreboard = document.getElementById('scoreboard');
 
 // UI Functions & Events
-button1.addEventListener('click', () => {
-  if (!gameEnded) {
+// Här är knappen jag klickar på för att få poäng.
+button1.addEventListener('click', () => { // Här ökas poängen för varje klick.
+  if (!gameEnded) { // (!gameEnded = om spelet fortfarande är igång).
     increaseScore();
   }
-
+// Här startas spelet och timern går igång.
   if (!gameStarted) {
     startGame();
   }
 })
 
+// Här skickas namnet och poängen in när submit-knappen klickas.
 button2.addEventListener('click', () => {
   submitHighScore();
 })
 
+// Här döljs namnfält, label och submit-knapp tills spelet är slut.
 input1.style.display = 'none';
 label1.style.display = 'none';
 button2.style.display = 'none';
 
 // Functions
+// Här ökas poängen för varje klick.
 function increaseScore() {
-  score++;
-  scoreDisplay.innerText = score;
+  score++;   // Här ser jag till att poängen ska ökas en gång per klick.
+  scoreDisplay.innerText = score; // Här visas mina poäng just nu.
 }
 
+// Här är timern.
 function countdown() {
-  timeLeft--;
-  timerDisplay.innerText = timeLeft;
-
+  timeLeft--;   // Här minskas tiden med en sekund i taget.
+  timerDisplay.innerText = timeLeft;   // Här syns tiden (uppdaterad) på sidan.
+  // Här stoppas spelet när det är 0 sekunder kvar.
   if (timeLeft <= 0) {
     timerDisplay.innerText = 0;
     endGame();
   }
+  // TODO: Sida?
 }
 
+// Här säger jag vad som ska ske för att starta spelet.
 function startGame() {
+  // Här sätts nedräkningen igång och körs varje sekund.
   interval = setInterval(countdown,  1000);
-  gameStarted = true;
+  // Här ser jag till att spelet läses som redan startat så att det inte börjar om.
+  gameStarted = true; // Här säger jag att spelet ska sättas som startat så att det inte startas flera gånger.
 }
 
+// Här markerar jag spelet som avslutat när tiden är slut.
 function endGame() {
-  gameEnded = true;
-  clearInterval(interval);
-  button1.style.display = 'none';
-  input1.style.display = 'block';
-  label1.style.display = 'block';
-  button2.style.display = 'block';
+  gameEnded = true; // Här sättar jag spelet som avslutat.
+  clearInterval(interval);   // Här stoppas timern.
+  scoreDisplay.innerText = `Final score: ${score}`;   // Här visas min slutliga poäng när spelet är klart.
+  button1.style.display = 'none'; // // Här göms click me-knappen efter att tiden är slut. (disabled = true hade
+  // inneburit att knappen syntes men inte skulle gå att klicka på.)
+  // Här visas input och submit-knapp så att jag kan spara och skicka in mina poäng och namn.
+  input1.style.display = 'block'; // Här blir namnfältet synligt.
+  label1.style.display = 'block'; // Här visas texten "Name (3 to 16 characters):"
+  button2.style.display = 'block'; // Här visas submit-knappen.
+  getScoreBoardData(); // Här hämtas den uppdaterade scoreboard när spelet är slut.
+  // TODO: Addera spärr input
+
 }
 
+// Här skapar jag en asynkron funktion som gör en POST-request för att skicka in min data och väntar på att requesten
+// ska bli klar.
 async function submitHighScore() {
-  const response = await fetch("https://hooks.zapier.com/hooks/catch/8338993/ujs9jj9/", {
-    method: "POST",
-    body: JSON.stringify({
+  const response = await fetch("https://hooks.zapier.com/hooks/catch/8338993/ujs9jj9/", { // await
+    // fetch skickar in min totala poäng.
+    method: "POST", // Här säger jag att min data ska skickas (en POST request).
+    body: JSON.stringify({     // Här görs namn och poäng om till JSON innan det skickas.
       name: input1.value,
       score: score
     }),
   });
-
   console.log(response);
+  getScoreBoardData(); // Här hämtas scoreboarden först efter att min data skickats/Post requesten är klar.
 }
-  // TODO: Addera spärr input
 
+// Här skapar jag en funktion som gör en GET request med fetch för att hämta scoreboarden.
+function getScoreBoardData() {
+  const url = 'https://script.google.com/macros/s/AKfycbys5aEPMvNCutyhNYYCcQcCjzsi2UtqNspmKyCH-AicJxJbCJMrAoT0LUaYaXhTWA8n/exec';
+  // Här sker GET requesten från url:en ovan.
+  fetch(url)
+    .then(response => {
+      console.log('Response object:', response); // Här loggas "svaret" för att kunna debuggas.
+      return response.json(); // Här säger jag att svaret ska göras om till JSON (för att kunna användas i JS).
+    })
+    .then(data => {
+      console.log('Scoreboard data:', data); // Här loggas scoreboard-datan.
+      scoreboard.innerHTML = ""; // Här rensas den gamla scoreboarden innan den nya skrivs ut.
+      // TODO: Sortera listan baserat på poäng innan loopen körs så index användas för att visa placering.
+      data.forEach((player, index) => { // Här säger jag att funktionen ska loopa genom alla spelare i scoreboarden.
+        scoreboard.innerHTML += `<p>${player.name}: ${player.score}</p>`; // Här görs varje namn och poäng synlig.
+      });
+    })
+    .catch(error => {
+      console.error('Fetch error:', error); // Här loggas och visas eventuella fel på fetchen.
+    });
+}
